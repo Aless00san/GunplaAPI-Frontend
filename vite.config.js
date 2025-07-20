59e51513d8428fe9d4300463745c2a1a79f5d4c8
Aless00san <mumaitronzx@gmail.com>
2025-07-20 12:16:48 +0200

    base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://https://main.dfnl6fna5gler.amplifyapp.com:5174',
+  base: '/',
+  // base: "http://localhost:5174",
 });
